5ed90a035f6fe9f207d71cd765ed7bc96abf58bf
Russ Parmer <rparmer02@gmail.com>
2013-03-22 20:23:55 -0700

    changed some webform settings

--- themes/touch/webform-form-14.tpl.php
+++ themes/touch/webform-form-14.tpl.php
@@ -36,24 +36,10 @@
  	
  </div>
  
-  <?php
-
- 
-
-   // Print out the main part of the form.
-
-   // Feel free to break this up and move the pieces within the array.
-
- 
+<?php
 
    print drupal_render($form['submitted']);
-
- 
-
-   // Always print out the entire $form. This renders the remaining pieces of the
-
-   // form that haven't yet been rendered above.
-
    print drupal_render_children($form);
+
 ?>
  
