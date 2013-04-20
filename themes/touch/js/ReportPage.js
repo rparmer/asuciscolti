926df6decf0b04e43d85802af2f7060df7903e0d
Pink Jazz <smoothpinkjazz@gmail.com>
2013-04-20 15:28:15 -0700

    Created first reports.

---
+++ themes/touch/js/ReportPage.js
@@ -0,0 +1,21 @@
+function listAllContent() {
+	(function($) {
+		$('#reportsData').html('<table width="100%" 		cellspacing="0" cellpadding="0" id="data-entry"></table>');
+		$('#data-entry').dataTable({
+			"bProcessing": true,
+			"bScrollInfinite": true,
+			"sScrollY": "1000px",
+			"sDom": '<"top">rt<"bottom">',
+			"aaSorting": [],
+			"sAjaxSource": "/drupal/API/Report/listAllContentPull.php",
+			"aoColumns": [
+				{ "sTitle": "Name" },
+				{ "sTitle": "Consumer key" },
+				{ "sTitle": "Shared secret" },
+				{ "sTitle": "Domain" },
+				{ "sTitle": "Privacy" },
+				{ "sTitle": "Submitted" }
+			]
+		});
+	})(jQuery);
+}
