8e33105465f52647824628f1bee0058d267015df
Russ Parmer <rparmer02@gmail.com>
2013-04-27 14:05:24 -0700

    corrected suggested lti page layout, added css for suggested lti elements

--- themes/touch/js/DataBaseSuggestLTI.js
+++ themes/touch/js/DataBaseSuggestLTI.js
@@ -6,17 +6,15 @@
                 $('#SuggestedLTI').html('<table width="100%" cellspacing="0" cellpadding="0" id="data-entry"></table>');
                 $('#data-entry').dataTable({
 	                "bProcessing": true,
-		
-                       "sDom": '<"inline"l><"floatright"f>rt<ip>',
-						"sAjaxSource": "/drupal/API/DataBaseSuggestLTIPull.php",
-                        "aoColumns": [
-						{ "sTitle": "LTI Name ", "sWidth": "40px" },
-						{ "sTitle": "URL", "sWidth": "50px" },
-						{ "sTitle": "Description", "sWidth": "50px" },
+                    "sDom": '<"inline"l><"floatright"f>rt<ip>',
+					"sAjaxSource": "/drupal/API/DataBaseSuggestLTIPull.php",
+                    "aoColumns": [
+						{ "sTitle": "LTI Name ", "sWidth": "60px" },
+						{ "sTitle": "URL", "sWidth": "60px" },
+						{ "sTitle": "Description", "sWidth": "60px" },
                         { "sWidth": "18px" }
-                         ]
+                    ]
                 });
-
             }
         }
     })(jQuery);
